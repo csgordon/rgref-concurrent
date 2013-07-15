69ed4b242168e916786f1d64e0d6e98d71ee52f1
Colin Stebbins Gordon <csgordon@cs.washington.edu>
2013-07-15 14:33:48 -0700

    reorg & optimize compilation

--- compile.sh
+++ compile.sh
@@ -2,10 +2,10 @@
 export CORE="RGref/DSL/LinearEnv.v RGref/DSL/DSL.v RGref/DSL/Theories.v \
              RGref/DSL/Core.v RGref/DSL/Monad.v RGref/DSL/Fields.v RGref/DSL/Concurrency.v"
 export EXAMPLES="AppendOnlyLinkedList.v MonotonicCounter.v PrependOnlyPureList.v \
-                 CounterModule.v RCC.v ReferenceImmutability.v LinkedList.v \
-                 AtomicCounter.v TrieberStack.v"
+                 CounterModule.v RCC.v ReferenceImmutability.v LinkedList.v"
+export CONC_EXAMPLES="AtomicCounter.v TrieberStack.v"
 export BUGS="KnownUnsoundnessExamples.v"
-coq_makefile -arg -impredicative-set -R RGref RGref $CORE $EXAMPLES $BUGS > Makefile
+coq_makefile -arg -impredicative-set -R RGref RGref $CORE $EXAMPLES $CONC_EXAMPLES $BUGS > Makefile
 touch .depend
 make depend
-make
+make -j `nproc`
