18f69337a80e1e36c682f61f7344fdba289298df
Colin Stebbins Gordon <csgordon@cs.washington.edu>
2013-07-23 10:10:02 -0700

    Switch Hindsight to axiomatic induction-induction

--- Hindsight.v
+++ Hindsight.v
@@ -54,55 +54,70 @@ Axiom ii_lt_trans : forall x y z, x ≪ y -> y ≪ z -> x ≪ z.
      IMPL = implicit in data representation (e.g. option vs. not option for nullable vs. non-null )
 
   *)
-  Inductive E : Set :=
-    mkE : forall C P R G,
-            ⊠ -> bool -> option (ref{C|P}[R,G]) -> E.
-  Definition valOfE (e:E) : ⊠ := match e with mkE C P R G n m tl => n end.
-  Definition markedOfE (e:E) : bool := match e with mkE C P R G n m tl => m end.
-  Inductive deltaE (P:hpred E): hrel E :=
+(** We'll axiomatize an inductive-inductive characterization, including elimination and the computational behavior
+    of the eliminator. *)
+  Axiom E : Set.
+  Axiom deltaE : hrel E.
+  Axiom invE : hpred E.
+  Axiom mkE : ⊠ -> bool -> option (ref{E|invE}[deltaE,deltaE]) -> E.
+  Axiom destruct_E : forall (e:E), exists n m tl, e = mkE n m tl.
+  Axiom E_rect : forall (T:E->Type) (body:forall n m tl, T (mkE n m tl)) (e:E), T e.
+  Axiom E_rect_red : forall T body n m tl, E_rect T body (mkE n m tl) = body n m tl.
+  Axiom mkE_inj : forall n n' m m' tl tl', mkE n m tl = mkE n' m' tl' -> n=n' /\ m=m' /\ tl=tl'.
+  Ltac injectE :=
+    match goal with
+        | [ H : mkE _ _ _ = mkE _ _ _ |- _ ] => let a := fresh in let b := fresh in let c := fresh in
+                                                destruct (mkE_inj _ _ _ _ _ _ H) as [a [b c]]; clear H
+    end.
+  Definition valOfE (e:E) : ⊠ := E_rect (fun _ => ⊠) (fun n m tl => n) e.
+  Definition markedOfE (e:E) : bool := E_rect (fun _ => bool) (fun n m tl => m) e.
+  Inductive deltaE' : hrel E :=
   (* Implicitly:
      - δk : The key of a node does not change
      - δen : successor of exterior (marked) node doesn't change
   *)
-  | deltaE_refl : forall R G n m nxt h h',
+  | deltaE_refl : forall n m nxt h h',
                     (match nxt with None => True | Some tl => valOfE (h'[tl]) = valOfE (h[tl]) end) ->
-                    deltaE P (mkE E P R G n m nxt) (mkE E P R G n m nxt) h h'
+                    deltaE' (mkE n m nxt) (mkE n m nxt) h h'
   (* δm : A marked node does not become unmarked (we encode the complement of this relation) *)
-  | deltaE_mark : forall n R G next h h',
+  | deltaE_mark : forall n next h h',
                     (valOfE (h'[next]) = valOfE (h[next])) ->
-                    deltaE P (mkE E P R G n false (Some next)) (mkE E P R G n true (Some next)) h h'
+                    deltaE' (mkE n false (Some next)) (mkE n true (Some next)) h h'
   (* δbn : if a backbone successor changes, it remains a backbone (unmarked) in the new heap
            (part 1 of 2: also need to support removal) *)
-  | deltaE_insert : forall n R G tl tl' h h' n',
+  | deltaE_insert : forall n tl tl' h h' n',
                       (* only insert unmarked nodes *)
-                      h'[tl']=(mkE E P R G n' false (Some tl)) -> (* note final heap *)
+                      h'[tl']=(mkE n' false (Some tl)) -> (* note final heap *)
                       n ≪ n' ->
-                      deltaE P (mkE E P R G n false (Some tl)) (mkE E P R G n false (Some tl')) h h' (* can assume Some b/c of sentinels *)
+                      deltaE' (mkE n false (Some tl)) (mkE n false (Some tl')) h h' (* can assume Some b/c of sentinels *)
    (* δbn : if a backbone successor changes, it remains a backbone (unmarked) in the new heap
             (part 2 of 2: see insertion above) *)
-  | deltaE_remove : forall R G n tl n' tl' h h',
+  | deltaE_remove : forall n tl n' tl' h h',
                       n ≪ n' -> (* should be easy to prove at writes, handy to have explicit for stability proof *)
                       (* only remove marked nodes *)
-                      h[tl] = (mkE E P R G n' true (Some tl')) -> (* note initial heap *)
-                      deltaE P (mkE E P R G n false (Some tl)) (mkE E P R G n false (Some tl')) h h'
-  .
-  Inductive invE : hpred E :=
-    pf_invE : forall h n m P next,
+                      h[tl] = (mkE n' true (Some tl')) -> (* note initial heap *)
+                      deltaE'  (mkE n false (Some tl)) (mkE n false (Some tl')) h h'
+  with invE' : hpred E :=
+    pf_invE : forall h n m next,
                 (* φ_< : The key of every node is smaller than the key of its successor *)
                 (match next with None => True | Some tl => exists n', n ≪ n' /\ valOfE (h[tl])=n' end) ->
-                invE (mkE E P (deltaE P) (deltaE P) n m next) h.
+                invE' (mkE n m next) h.
+  (* Let's try just baking equality in since we're equating elements of Prop anyways, rather than the iff used in other examples. *)
+  Axiom inv : invE = invE'.
+  Axiom delt : deltaE = deltaE'.
+  Ltac fixdefs := try rewrite inv in *; try rewrite delt in *.
 
   Inductive head_props : hpred E :=
     (* φ-∞ : Key of the head is -∞ *)
-    head_props_ctor : forall h tl, head_props (mkE E invE (deltaE invE) (deltaE invE) -∞ false (Some tl)) h.
+    head_props_ctor : forall h tl, head_props (mkE -∞ false (Some tl)) h.
   Inductive tail_props : hpred E :=
     (* φTn : The tail node has no successor
        φ∞ : Key of the tail is ∞ *)
-    tail_props_ctor : forall h, tail_props (mkE E invE (deltaE invE) (deltaE invE) ∞ false None) h.
+    tail_props_ctor : forall h, tail_props (mkE ∞ false None) h.
   Inductive HindsightListBlock : Set :=
     (* φH, φT: head and tail are non-null, encoded as not being options *)
-    mkHLB : ref{E|(invE ⊓ head_props)}[deltaE invE,deltaE invE] -> 
-            ref{E|(invE ⊓ tail_props)}[deltaE invE,deltaE invE] -> 
+    mkHLB : ref{E|(invE ⊓ head_props)}[deltaE,deltaE] -> 
+            ref{E|(invE ⊓ tail_props)}[deltaE,deltaE] -> 
             HindsightListBlock.
 
   (* The actual hindsight list.
@@ -114,49 +129,23 @@ Axiom ii_lt_trans : forall x y z, x ≪ y -> y ≪ z -> x ≪ z.
     nextOfE : E -> Maybe (Ref E invE deltaE deltaE)
     nextOfE (mkE n m next) = next*)
 
-  Definition eptr := ref{E|invE}[deltaE invE,deltaE invE].
+  Definition eptr := ref{E|invE}[deltaE,deltaE].
 
-  (** Unfortunately, using the impredicative encoding for mutually inductive datatypes with mutual indexing
-     means injectivity can't work on equalities between Es: it would produce an equality on
-     two elements of Set, which is in Type, thereby performing a large elimination into a predicative
-     universe.  To work around this, I'm adding injectivity axioms for E.
-
-     THIS IS UNSOUND.
-
-     But the ways I want to use them should not actually produce contradictions, and I'm working around
-     an implementation limitation of Coq's support for mutual inductive types. *)
-  Axiom e_inj1 : forall C D P P' R R' G G' n n' m m' tl tl', mkE C P R G n m tl = mkE D P' R' G' n' m' tl' -> C = D.
-  (* Technically this axiom also adds functional extensionality and proof irrelevance in Prop... *)
-  Axiom e_inj2 : forall C P P' R R' G G' n n' m m' tl tl', mkE C P R G n m tl = mkE C P' R' G' n' m' tl' -> P = P' /\ R = R' /\ G = G'.
-  Axiom e_inj3 : forall C P R G n n' m m' tl tl', mkE C P R G n m tl = mkE C P R G n' m' tl' -> n=n' /\ m=m' /\ tl=tl'.
-
-  Ltac crunchE :=
-    repeat match goal with
-    | [ h : mkE ?c ?p ?r ?g ?n ?m ?tl = mkE ?d ?q ?s ?t ?n2 ?m2 ?tl2 |- _ ] => let H := fresh in
-                                                                                 let H' := fresh in
-                                                                                   let H'' := fresh in
-                                                                                   destruct (e_inj3 _ _ _ _ _ _ _ _ _ _ h) as [H [H' H'']]; clear h; subst
-    | [ h : mkE ?c ?p ?r ?g ?n ?m ?tl = mkE ?d ?q ?s ?t ?n' ?m' ?tl' |- _ ] => let H := fresh in
-                                                                                 let H' := fresh in
-                                                                                   let H'' := fresh in
-                                                                                   destruct (e_inj2 _ _ _ _ _ _ _ _ _ _ _ _ _ h) as [H [H' H'']]; subst
-    | [ h : mkE ?c ?p ?r ?g ?n ?m ?tl = mkE ?d ?q ?s ?t ?n' ?m' ?tl' |- _ ] => let H := fresh in
-                                                                                 let H' := fresh in
-                                                                                   let H'' := fresh in
-                                                                                   destruct (e_inj1 _ _ _ _ _ _ _ _ _ _ _ _ _ _ h) as [H [H' H'']]; subst
-    end.
-  Unset Ltac Debug.
-
-  Lemma stable_nodes : stable invE (deltaE invE).
+  Lemma stable_nodes : stable invE deltaE.
   Proof.
+    fixdefs.
     red; intros.
-    induction H0; inversion H; subst; crunchE; eauto.
+    induction H0; inversion H; repeat injectE; subst; eauto.
         (*refl*) induction nxt; constructor; eauto.
-                 destruct H1. destruct H1. exists x. intuition. rewrite H0; eauto.
-        (*mark*) destruct H1; destruct H1; eexists; intuition. exists x. rewrite <- H2 in *. eauto. 
-        (*ins*) destruct H2. destruct H2. constructor. exists n'. intuition. rewrite H0. compute. reflexivity.
+                 destruct H2. destruct H1. exists x. intuition. rewrite H0; eauto.
+        (*mark*) destruct H2; destruct H1; eexists; intuition. exists x. rewrite <- H2 in *. eauto. 
+        (*ins*) destruct H3. destruct H2. constructor. exists n'. intuition. rewrite H0. compute.
+                 rewrite E_rect_red.
+                 reflexivity.
         (*rm *) constructor.
-                 assert (Htmp := heap_lookup2 h' tl'). inversion Htmp; subst.
+                 assert (Htmp := heap_lookup2 h' tl').
+                 assert (Htmp' : invE' (h' [tl']) h'). rewrite <- inv. assumption.
+                 clear Htmp. inversion Htmp'; subst.
                  exists n'; intuition.
                  (* The issue is that we don't presently have any explicit connection between h[tl'] and h'[tl'].
                     They should either be the same (in this case definitely, but proving the acyclicity etc. is hard)
@@ -171,28 +160,52 @@ Axiom ii_lt_trans : forall x y z, x ≪ y -> y ≪ z -> x ≪ z.
                  exists x0. split. apply (ii_lt_trans n x); eauto.*)
   Admitted.
   Hint Resolve stable_nodes.
-  Lemma stable_tail : stable tail_props (deltaE invE).
+  Lemma stable_tail : stable tail_props deltaE.
   Proof.
+    fixdefs.
     red; intros.
     Require Import Coq.Program.Equality.
-    dependent induction H0; inversion H; subst; crunchE; eauto; try constructor.
-    (* This case should be by [inversion H.] since tail_props prohibits the non-null next pointer in H... impred-set *) admit.
-    (* Again by inversion... *) admit.
-    (* And again... *) admit.
+    dependent induction H0; inversion H; subst; repeat injectE; subst; eauto; try constructor.
+    inversion H4. inversion H5. (* <-- Can't append when next is already Some *)
+    inversion H4. inversion H5. 
   Qed.
   Hint Resolve stable_tail.
+  Lemma stable_head : stable head_props deltaE.
+  Proof.
+    fixdefs.
+    red; intros.
+    Require Import Coq.Program.Equality.
+    dependent induction H0; inversion H; subst; repeat injectE; subst; eauto; try constructor.
+    inversion H4; subst.
+    (* IMPORTANT: This is the mark case of deltaE, which permits marking the head.
+       (Marking the tail is prohibited because we only mark nodes with (Some _) tails.)
+       We shouldn't be allowed to mark the head for removal. Doing so also breaks linearizability,
+       which is probably why the Hindsight paper doesn't enforce this constraint.
+       TODO: Is there some way to spin this as an important clarification or minor bug fix to
+             the Hindsight paper?  How do I present this.  Is the stability just irrelevant
+             for the Hindsight paper, since they observe that all actions fall within those
+             steps, and preserve these properties (since they don't mark the head)?
+       TODO: Make sure this isn't just me doing a bad job translating the Hindsight paper's invariants.
+     *)
+  Admitted.
+  Hint Resolve stable_head.
     
-
-  Inductive e_reaching (T:Set) P R G : ref{T|P}[R,G] -> E -> Prop :=
-    tl_ptr_reach : forall n m r, e_reaching T P R G r (mkE T P R G n m (Some r)).
+  Inductive e_reaching : forall (T:Set) P R G, ref{T|P}[R,G] -> E -> Prop :=
+    tl_ptr_reach : forall n m r, e_reaching _ _ _ _ r (mkE n m (Some r)).
   Global Instance e_reach : ImmediateReachability E := { imm_reachable_from_in := e_reaching }.
   Global Instance e_cont : Containment E :=
-  { contains := (fun Rel => forall T P (R G:hrel T) n m r h h', R (h[r]) (h'[r]) h h' -> Rel (mkE T P R G n m (Some r)) (mkE T P R G n m (Some r)) h h') }.
+  { contains := (fun Rel =>
+                   forall n m R (r:ref{E|invE}[deltaE,deltaE]) h h',
+                     R (h[r]) (h'[r]) h h' ->
+                     Rel (mkE n m (Some r)) (mkE n m (Some r)) h h') }.
   Print rel_fold.
+  (* The new ind-ind trick loses the ability to weaken every structural member of an inductive type,
+     so this needs the pending folding update to treat only reflexive folds and field-access folds. *)
   Program Instance fold_e : rel_fold E :=
   { rgfold := (fun R G => E);
-    fold := (fun R' G' e => match e with
-                          | mkE T P R G n m None =>
+    fold := (fun R' G' e => e
+                              (*match e with
+                          | mkE n m None =>
                               mkE T P R (fun x x' h h' => G x x' h h' /\
                                                             forall n' m' r, h[r]=x -> h'[r]=x' ->
                                                               G' (mkE T P R G n' m' (Some r)) (mkE T P R G n' m' (Some r)) h h') n m None
@@ -201,14 +214,12 @@ Axiom ii_lt_trans : forall x y z, x ≪ y -> y ≪ z -> x ≪ z.
                                                             (forall n' m' r (ir:ImmediateReachability T), h[r]=x -> h'[r]=x' ->
                                                               G' (mkE T P R G n' m' (Some r)) (mkE T P R G n' m' (Some r)) h h'))
                                   n m (Some (@convert_G T P R G _ _ _ _ tl))
-                          end)
+                          end*) )
   }.
-  Next Obligation. Admitted. (* Not sure where to find an ImmReach...*)
-  Next Obligation. red; intros. Admitted. (* Should be precise... need to prove this eventually *)
-  Next Obligation. red. intros. intuition. Defined.
 
   Lemma precise_invE : precise_pred invE.
   Proof.
+    fixdefs.
     red. intros. inversion H. induction next; subst; try solve[constructor; eauto].
     constructor. rewrite <- H0. assumption. constructor. constructor.
   Qed.
@@ -222,8 +233,9 @@ Axiom ii_lt_trans : forall x y z, x ≪ y -> y ≪ z -> x ≪ z.
     constructor.
   Qed.
   Hint Resolve precise_invE precise_tail precise_head.
-  Lemma precise_deltaE : precise_rel (deltaE invE).
+  Lemma precise_deltaE : precise_rel deltaE.
   Proof.
+    fixdefs.
     red; intros. inversion H1; subst; try constructor.
     induction nxt; eauto. rewrite <- H0. rewrite <- H. eauto. repeat constructor. repeat constructor.
     rewrite <- H0. rewrite <- H. eauto. repeat constructor. repeat constructor.
@@ -258,18 +270,31 @@ Axiom ii_lt_trans : forall x y z, x ≪ y -> y ≪ z -> x ≪ z.
 
    *)
   Inductive r_hlb : forall T P R G (r:ref{T|P}[R,G]), HindsightListBlock -> Prop :=
-    | r_hd : forall hd tl, r_hlb E (invE ⊓ head_props) (deltaE invE) (deltaE invE) hd (mkHLB hd tl)
-    | r_tl : forall hd tl, r_hlb E (invE ⊓ tail_props) (deltaE invE) (deltaE invE) tl (mkHLB hd tl).
+    | r_hd : forall hd tl, r_hlb E (invE ⊓ head_props) deltaE deltaE hd (mkHLB hd tl)
+    | r_tl : forall hd tl, r_hlb E (invE ⊓ tail_props) deltaE deltaE tl (mkHLB hd tl).
   Global Instance reach_hlb : ImmediateReachability HindsightListBlock := { imm_reachable_from_in := r_hlb }.
   Instance fold_HLB : rel_fold HindsightListBlock. Admitted.
                                                      
            
   Program Definition init_HL {Γ}(_:unit) : rgref Γ hindsight_list Γ :=
-    tail <- Alloc (mkE E invE (deltaE invE) (deltaE invE) ∞ false None);
-    head <- Alloc (mkE E invE (deltaE invE) (deltaE invE) -∞ false (Some (convert_P _ _ _ tail)));
+    tail <- Alloc (mkE ∞ false None);
+    head <- Alloc (mkE -∞ false (Some (convert_P _ _ _ tail)));
     Alloc (mkHLB head tail).
-  Next Obligation. split; try constructor; eauto. Qed. (* Tail P *)
+  Next Obligation.
+    split; try constructor; eauto.
+    assert (forall x h, invE' x h -> invE x h). fixdefs. auto.
+    apply H0. constructor; eauto.
+  Qed. (* Tail P *)
   Next Obligation. eapply pred_and_proj1; eauto. Qed.
+  Next Obligation.
+    assert (forall x h, invE' x h -> invE x h). fixdefs. auto.
+    split; intuition; eauto.
+    apply H0. constructor.
+    rewrite conversion_P_refeq.
+    assert (Htmp := heap_lookup2 h tail). destruct Htmp. inversion H2; subst.
+    exists ∞. intuition; eauto. apply ninf_lt_inf. compute. rewrite E_rect_red. reflexivity.
+    constructor.
+  Qed.
     
     {- TODO: I feel like the return type might need to be refined to give the relationship between k and the node values -}
     locate : hindsight-list → ⊠ → ● (pair eptr eptr)
