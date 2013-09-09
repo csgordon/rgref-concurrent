36216e337741374d0ec337f6a7599468109f01e6
Colin Stebbins Gordon <csgordon@cs.washington.edu>
2013-09-09 12:57:20 -0700

    refactor Fin.t lemmas

--- UnionFind.v
+++ UnionFind.v
@@ -1,6 +1,7 @@
 Require Import RGref.DSL.DSL.
 Require Import RGref.DSL.Concurrency.
 Require Import RGref.DSL.Fields.
+Require Import FinResults.
 
 (** * Lock-Free Linearizable Union-Find
     We're following Anderson and Woll's STOC'91 paper 
@@ -25,68 +26,6 @@ Instance cell_rank {n:nat} : FieldType (cell n) F rank nat :=
 Instance cell_parent {n:nat} : FieldType (cell n) F parent (Fin.t n) :=
   { getF := fun x => match x with mkCell r p => p end;
     setF := fun x v => match x with mkCell r p => mkCell _ r v end }.
-(** ** A few useful results about finitely-bounded natural numbers *)
-Section FinResults.
-
-Definition fin_beq {n:nat} (x y:Fin.t n) : bool :=
-  Fin.rect2 (fun _ _ _ => bool)
-            (* F1 F1 *) (fun _ => true)
-            (* F1 FS *) (fun _ _ => false)
-            (* FS F1 *) (fun _ _ => false)
-            (* FS FS *) (fun _ _ _ rec => rec) x y.
-
-Lemma fin_beq_eq : forall n x y, @fin_beq n x y = true <-> x = y.
-Proof.
-  intros. split; intros.
-  Require Import Coq.Program.Equality.
-  (* -> *) induction x; dependent induction y; try reflexivity; try inversion H.
-           rewrite (IHx y H1). reflexivity.
-  (* <- *) induction x. inversion y; subst; simpl; reflexivity.
-                        inversion y; subst. unfold fin_beq in *. simpl in *. eapply IHx. reflexivity.
-                        unfold fin_beq in *; simpl in *. eapply IHx. reflexivity.
-Qed.
-Definition fin_lt {n:nat} (x y:Fin.t n) : bool :=
-  Fin.rect2 (fun _ _ _ => bool)
-            (* F1 F1 *) (fun _ => false)
-            (* F1 FS *) (fun _ _ => true)
-            (* FS F1 *) (fun _ _ => false)
-            (* FS FS *) (fun _ _ _ rec => rec) x y.
-Program Lemma proj1_to_nat_comm : forall n (x:t n), proj1_sig (to_nat (FS x)) = S (proj1_sig (to_nat x)).
-Proof.
-  intros.
- induction x. compute. reflexivity. rewrite IHx. 
- simpl. induction (to_nat x). simpl. reflexivity.
-Qed.
-Program Lemma fin_lt_nat : forall n (x y:Fin.t n), @fin_lt n x y = true <-> (to_nat x) < to_nat y.
-Proof.
-  intros. split.
-  Check Fin.rect2.
-  eapply Fin.rect2 with (a := x) (b := y); intros.
-      inversion H. 
-      induction f. simpl. auto with arith. rewrite proj1_to_nat_comm. compute. auto with arith.
-      inversion H.
-      induction f. repeat rewrite proj1_to_nat_comm. auto with arith.
-      rewrite proj1_to_nat_comm. rewrite (@proj1_to_nat_comm _ g).
-      auto with arith.
-  (* <- *)
-  eapply Fin.rect2 with (a := x) (b := y); intros; try solve[inversion H].
-      compute; auto.
-      simpl. apply H. repeat rewrite proj1_to_nat_comm in H0. inversion H0. constructor. subst. auto with arith.
-Qed.
-Lemma fin_dec : forall n (x y : t n), {x=y}+{not (x=y)}.
-  intros. eapply Fin.rect2 with (a := x) (b := y); intros.
-  left. reflexivity. right. discriminate.
-  right. discriminate. 
-  induction H. subst. left; auto.
-  right. red in b. red. intros. apply b.
-  inversion H.
-  assert (projT1 (existT (fun n => t n) n0 f) = projT1 (existT (fun n => t n) n0 g)).
-      compute. auto.
-  eapply Eqdep_dec.inj_pair2_eq_dec. apply Peano_dec.eq_nat_dec.
-  apply H1.
-Qed.
-
-End FinResults.
   
 (** ** Useful predicates and properties of the union-find array structure *)
 Inductive root (n:nat) (x:uf n) (h:heap) (i:Fin.t n) : Fin.t n -> Prop :=
--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export AUX="FinResults.v"
 export CORE="RGref/DSL/LinearEnv.v RGref/DSL/DSL.v RGref/DSL/Theories.v \
              RGref/DSL/Core.v RGref/DSL/Monad.v RGref/DSL/Fields.v RGref/DSL/Concurrency.v"
 export EXAMPLES="AppendOnlyLinkedList.v MonotonicCounter.v PrependOnlyPureList.v \
@@ -6,7 +7,7 @@ export EXAMPLES="AppendOnlyLinkedList.v MonotonicCounter.v PrependOnlyPureList.v
 export CONC_EXAMPLES="AtomicCounter.v TrieberStack.v MichaelScottQ.v Hindsight.v UnionFind.v"
 export BUGS="KnownUnsoundnessExamples.v"
 export TMP="Scratch.v"
-coq_makefile -R RGref RGref $TMP $CORE $EXAMPLES $CONC_EXAMPLES $BUGS > Makefile
+coq_makefile -R RGref RGref $TMP $AUX $CORE $EXAMPLES $CONC_EXAMPLES $BUGS > Makefile
 touch .depend
 make depend
 make -j `nproc`
